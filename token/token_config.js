f5c6bb04bb65717fbf7a05572aadef38a2651bee
Sean Pollock <seanpollock3344@gmail.com>
2017-08-28 14:45:20 -0400

    defining token params type and exporting config

---
+++ token/token_config.js
@@ -0,0 +1,39 @@
+type tokenParams = {
+  name: string,
+  ticker: string,
+  decimal: number,
+  supply: number
+} 
+
+export const tokens: Array<tokenParams> = [
+  {
+    'name': 'Mob',
+    'ticker': 'MOB',
+    'decimal': 0,
+    'suppply': 1000000000,
+  },
+  {
+    'name': 'Basic Attention Token',
+    'ticker': 'BAT',
+    'decimal': 0
+    'suppply': 1000000000,
+  },
+  {
+    'name': 'Mob',
+    'ticker': 'MOB',
+    'decimal': 0,
+    'suppply': 1000000000,
+  },
+  {
+    'name': 'Mob',
+    'ticker': 'MOB',
+    'decimal': 0,
+    'suppply': 1000000000,
+  },
+  {
+    'name': 'Mob',
+    'ticker': 'MOB',
+    'decimal': 0,
+    'suppply': 1000000000,
+  }
+]
