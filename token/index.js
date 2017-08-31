491ab021d748aa454d5c6750e80575425bbf96d7
Sean Pollock <seanpollock3344@gmail.com>
2017-08-30 21:25:06 -0400

    wip. first commit

---
+++ token/index.js
@@ -0,0 +1,13 @@
+export default class Token {
+  constructor(params) {
+
+  }
+
+  compile(contract: string) {
+    const path = getFilePath(contract)
+  }
+
+  getFilePath(contract: string) {
+
+  }
+}
