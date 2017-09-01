271f2c2f0093855a1849637b404ab158cc1b9694
Sean Pollock <seanpollock3344@gmail.com>
2017-09-01 13:34:55 -0400

    primitive broadcast system working

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import ws from 'ws'
 import stringy from 'stringy'
 import crypto from 'crypto'
+import delay from 'await-delay'
 const sha256 = crypto.createHash('sha256');
 
 console.log('sha256', sha256)
@@ -29,7 +30,6 @@ wss.on('connection', async (socket, req) => {
 async function registerClient(socket) {
   try {
     const hash = sha256.update(stringy.stringify(socket)).digest('hex')
-    console.log('hash', hash)
     clients[hash] = socket
     socket.onmessage = (e) => {
       console.log(`Message from ${clients[hash]}`, e.data)
@@ -38,3 +38,26 @@ async function registerClient(socket) {
     console.log('### error while registering client', err)
   }
 }
+
+async function broadcast(msg) {
+  if(Object.keys(clients).length === 0) {
+    return true;
+  }
+  Promise.all(
+    Object.keys(clients).map(client => {
+      console.log('client', clients[client])
+      clients[client].send(msg)
+    })
+  ).catch(err => {
+    console.log('### error in broadcast', err)
+  })
+}
+
+async function cron() {
+  await delay(3000)
+  console.log('here')
+  await broadcast("broadcast from server")
+  cron()
+}
+
+cron()
