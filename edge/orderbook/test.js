be37d7277f70a795a8e4ef0ddc219190ea15e8b5
Sean Pollock <seanpollock3344@gmail.com>
2017-09-16 17:46:42 -0700

    insertion sort not working :(

--- edge/orderbook/test.js
+++ edge/orderbook/test.js
@@ -9,8 +9,8 @@ test('should submit one sell to ledger', async (t) => {
   const book = new Orderbook()
   try {
     t.plan(2)
-    await book.submitSellA(randomOrder)
-    await book.submitSellB(randomOrder)
+    await book.submitSellA(randomOrder())
+    await book.submitSellB(randomOrder())
     t.assert(book.sellA.size > 0)
     t.assert(book.sellB.size > 0)
   } catch (err) {
@@ -38,13 +38,13 @@ test('should submit 10 sells to ledger wile maintaining a sorted set', async (t)
 })
 
 async function submitSellAOrders(book) {
-  await book.submitSellA(randomOrder)
+  await book.submitSellA(randomOrder())
   orderABatch--
   if (orderABatch > 0) await submitSellAOrders(book)
 }
 
 async function submitSellBOrders(book) {
-  await book.submitSellB(randomOrder)
+  await book.submitSellB(randomOrder())
   orderBBatch--
   if (orderBBatch > 0) await submitSellBOrders(book)
 }
