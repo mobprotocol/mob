374d95923243021b129abeb7721fe7fc0a32ec77
Sean Pollock <seanpollock3344@gmail.com>
2017-09-16 17:16:02 -0700

    yess recursive await magic

--- edge/orderbook/test.js
+++ edge/orderbook/test.js
@@ -8,9 +8,9 @@ const randomOrder = {
   price: new BN(Math.floor(Math.random(101) + 1)),
   quantity: new BN(Math.floor(Math.random(101) + 1))
 }
-const book = new Orderbook()
 
-test('sellBaseCase', async (t) => {
+test('should submit one sell to ledger', async (t) => {
+  const book = new Orderbook()
   try {
     t.plan(2)
     await book.submitSellA(randomOrder)
@@ -18,24 +18,43 @@ test('sellBaseCase', async (t) => {
     t.assert(book.sellA.size > 0)
     t.assert(book.sellB.size > 0)
   } catch (err) {
-    console.log('### ERROR IN TEST', err)
+    console.log('### ERROR in base case test', err)
   }
 })
 
-function submitSellAOrders() {
-  return new Promise(async (res, rej) => {
-    console.log('randomOrder', randomOrder)
-    await book.submitSellA(randomOrder)
-    orderABatch--
-    return submitSellAOrders()
-  })
+test('should submit 10 sells to ledger wile maintaining a sorted set', async (t) => {
+  const book = new Orderbook()
+  try {
+    t.plan(1)
+    await submitSellAOrders(book)
+    console.log('book.sellA.size', book.sellA.size)
+    t.assert(book.sellA.size === 10)
+  } catch (err) {
+    console.log('### ERROR in sort test', err)
+  }
+})
+
+// function submitSellAOrders() {
+//   return new Promise(async (res, rej) => {
+//     console.log('randomOrder', randomOrder)
+//     await book.submitSellA(randomOrder)
+//     orderABatch--
+//     return submitSellAOrders()
+//   })
+// }
+
+async function submitSellAOrders(book) {
+  await book.submitSellA(randomOrder)
+  console.log('orderABatch', orderABatch)
+  orderABatch--
+  if (orderABatch > 0) await submitSellAOrders(book)
 }
 
-function submitSellBOrders() {
+function submitSellBOrders(book) {
   return new Promise(async (res, rej) => {
     if(orderBBatch <= 0) res(true)
     await book.submitSellB(randomOrder)
     orderBBatch--
-    return submitSellBOrders()
+    return submitSellBOrders(book)
   })
 }
