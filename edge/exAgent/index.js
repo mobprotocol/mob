31e5b220c7774726f2676d2d7302fd1747f62ce7
Sean Pollock <seanpollock3344@gmail.com>
2017-09-16 19:46:24 -0700

    small fixes, error handling

--- edge/exAgent/index.js
+++ edge/exAgent/index.js
@@ -2,18 +2,24 @@ import delay from 'await-delay'
 
 export default class ExAgent {
   constructor(params) {
-    this.orderbook = params.orderbook
+    this.book = params.orderbook
   }
 
   async daemon() {
-    await delay(3000)
-    await this.match()
-    await this.daemon()
+    try {
+      await delay(3000)
+      await this.match()
+      await this.daemon()
+    } catch (err) {
+      console.log('### ERROR in main daemon', err)
+    }
   }
 
   async match() {
     try {
-      const side = chooseSide()
+      const sides = this.chooseSide()
+      const order = this.book[`sell${sides[0]}`].last()
+      await this.fillOrder(sides, order)
     } catch (err) {
       console.log('### ERROR in match', err)
     }
@@ -21,14 +27,16 @@ export default class ExAgent {
 
   chooseSide() {
     const bnry = Math.round(Math.random)
-    bnry === 0 ? return 'A' : return 'B'
+    if (bnry === 0) return ['A', 'B']
+    else return ['B', 'A']
   }
 
-  getOrder() {
-
-  }
-
-  fillOrder() {
-
+  fillOrder(sides, order) {
+    try {
+      const inverseOrder = this.book[`sell${sides[1]}`].last()
+      console.log('inverseOrder', inverseOrder)
+    } catch (err) {
+      console.log('### ERROR in fillOrder', err)
+    }
   }
 }
--- edge/exAgent/test.js
+++ edge/exAgent/test.js
@@ -1,17 +1,22 @@
+import test from 'tape'
 import ExAgent from './index'
 import Orderbook from '../orderbook/index'
 
-// import {
-//   randomOrder,
+import {
+  randomOrder,
 //   submitSellAOrders,
 //   submitSellBOrders
-// } from '../orderbook/test'
+} from '../orderbook/test'
 
 const book = new Orderbook()
 const agent = new ExAgent({orderbook: book})
 
-async function test() {
-   await agent.daemon()
-}
-
-test()
+test('shold match two orders in the market', async (t) => {
+  try {
+    await book.submitSellA(randomOrder())
+    await book.submitSellB(randomOrder())
+    await agent.daemon()
+  } catch (err) {
+    console.log('### ERROR in test', err)
+  }
+})
