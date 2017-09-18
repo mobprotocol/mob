b0cf49d2b56872b3b34bcbec7e7fec373534fce1
Sean Pollock <seanpollock3344@gmail.com>
2017-09-18 11:28:43 -0700

    file read fn for user data

--- scripts/startRPC.js
+++ scripts/startRPC.js
@@ -1,11 +1,23 @@
 import rpc from 'ethereumjs-testrpc'
-import users from './createUsers.js'
+import fs from 'fs'
 
-users().then(usrs => {
+async function getUsersData() {
+  try {
+    return await fs.readFile('../conf/users.json')
+  } catch (err) {
+    console.log('### ERROR in getUsersData', err)
+  }
+}
+
+getUsersData().then(usrs => {
   const server = rpc.server({'accounts': usrs })
-
   server.listen('8545', (err, chain) => {
     if (err) console.log('### ERROR in testrpc server', err)
     console.log('chain', chain)
+  })
+
+  fs.writeFile('./conf/users.json', JSON.stringify(users), 'utf-8', err => {
+    if (err) console.log('### ERROR in users file write', err)
+    console.log('### Saved users to ./conf/users.json')
   })
 })
