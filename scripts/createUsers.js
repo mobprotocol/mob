daf5bba0931540aada6f013267ccf61941bce1a5
Sean Pollock <seanpollock3344@gmail.com>
2017-09-18 08:34:59 -0700

    private key parsing and padding

--- scripts/createUsers.js
+++ scripts/createUsers.js
@@ -3,8 +3,13 @@ import keys from 'keythereum'
 const params = { keyBytes: 32, ivBytes: 16 };
 
 async function createUser() {
-  const dk = await keys.create(params)
-  console.log('dk', dk)
+  try {
+    const privateKey = await keys.create(params).privateKey.toString('hex').replace(/^/,'0x')
+    console.log('privateKey', privateKey)
+    // const publicAddress
+  } catch (err) {
+    console.log('### ERROR in createUser', err)
+  }
 }
 
 createUser()
