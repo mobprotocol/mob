1f728be38c904b22b8f5e0cf639d3adb5e6d6229
Sean Pollock <seanpollock3344@gmail.com>
2017-09-18 09:09:44 -0700

    getting pubKey

--- scripts/createUsers.js
+++ scripts/createUsers.js
@@ -1,14 +1,17 @@
 import keys from 'keythereum'
-import { privateToAddress } from 'ethereumjs-util'
+import {
+  privateToAddress,
+  privateToPublic,
+} from 'ethereumjs-util'
 
 const params = { keyBytes: 32, ivBytes: 16 };
 
 async function createUser() {
   try {
     const privKey = await keys.create(params).privateKey.toString('hex').replace(/^/,'0x')
+    const pubKey = await privateToPublic(privKey).toString('hex').replace(/^/, '0x')
+    console.log('pubKey', pubKey)
     const pubAddress = await privateToAddress(privKey).toString('hex').replace(/^/, '0x')
-    console.log('publicAddresss', pubAddress)
-    // const publicAddress.
   } catch (err) {
     console.log('### ERROR in createUser', err)
   }
