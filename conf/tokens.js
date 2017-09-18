0557fa32b8b98d2676be78f60210d8b32023d577
Sean Pollock <seanpollock3344@gmail.com>
2017-09-18 12:17:00 -0700

    array of token params

---
+++ conf/tokens.js
@@ -0,0 +1,62 @@
+export const tokens = [
+  {
+    name: 'Mob',
+    ticker: 'MOB',
+    decimals: 10e9,
+    supply: 10e18,
+  },
+  {
+    name: 'OmiseGO',
+    ticker: 'OMG',
+    decimals: 10e18,
+    supply: 10e27,
+  },
+  {
+    name: 'Qtum',
+    ticker: 'QTUM',
+    decimals: 10e18,
+    supply: 10e26,
+  },
+  {
+    name: 'Basic Attention Token',
+    ticker: 'BAT',
+    decimals: 10e18,
+    supply: 15e27,
+  },
+  {
+    name: 'EOS',
+    ticker: 'EOS',
+    decimals: 10e18,
+    supply: 10e27,
+  },
+  {
+    name: 'Maker',
+    ticker: 'MKR',
+    decimals: 10e18,
+    supply: 10e24,
+  },
+  {
+    name: 'Golem',
+    ticker: 'QNT',
+    decimals: 10e18,
+    supply: 10e27,
+  },
+  {
+    name: '0xProject',
+    ticker: 'ZRX',
+    decimals: 10e18,
+    supply: 10e27,
+  },
+  {
+    name: 'StatusNetwork',
+    ticker: 'SNT',
+    decimals: 10e18,
+    supply: 68e27,
+  },
+  {
+    name: 'Aargon',
+    ticker: 'ANT',
+    decimals: 10e18,
+    supply: 39e27,
+  },
+]
