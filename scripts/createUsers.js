a0eec0adc1505871ac5eb88aa950644ee6bce837
Sean Pollock <seanpollock3344@gmail.com>
2017-09-18 09:06:07 -0700

    user creating script. getching private key and public address

--- scripts/createUsers.js
+++ scripts/createUsers.js
@@ -1,12 +1,14 @@
 import keys from 'keythereum'
+import { privateToAddress } from 'ethereumjs-util'
 
 const params = { keyBytes: 32, ivBytes: 16 };
 
 async function createUser() {
   try {
-    const privateKey = await keys.create(params).privateKey.toString('hex').replace(/^/,'0x')
-    console.log('privateKey', privateKey)
-    // const publicAddress
+    const privKey = await keys.create(params).privateKey.toString('hex').replace(/^/,'0x')
+    const pubAddress = await privateToAddress(privKey).toString('hex').replace(/^/, '0x')
+    console.log('publicAddresss', pubAddress)
+    // const publicAddress.
   } catch (err) {
     console.log('### ERROR in createUser', err)
   }
