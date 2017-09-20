521109ff2c860479788100fce97465946974cfa6
Sean Pollock <seanpollock3344@gmail.com>
2017-09-19 18:47:40 -0700

    ethjs provider

--- edge/orderbook/test.js
+++ edge/orderbook/test.js
@@ -9,43 +9,49 @@ import Eth from 'ethjs-query'
 const eth = new Eth(new provider('http://localhost:8545'))
 let orderABatch = 10
 let orderBBatch = 10
-const tokenA = ''
-const tokenB = ''
+const tokenA = '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07'
+const tokenB = '0x9a642d6b3368ddc662CA244bAdf32cDA716005BC'
 
-test('should submit one sell to ledger', async (t) => {
-  const book = new Orderbook()
-  try {
-    t.plan(2)
-    await book.submitSellA(randomOrder())
-    await book.submitSellB(randomOrder())
-    t.assert(book.sellA.size > 0)
-    t.assert(book.sellB.size > 0)
-  } catch (err) {
-    console.log('### ERROR in base case test', err)
-  }
-})
+// test('should submit one sell to ledger', async (t) => {
+//   const book = new Orderbook()
+//   try {
+//     t.plan(2)
+//     await book.submitSellA(randomOrder())
+//     await book.submitSellB(randomOrder())
+//     t.assert(book.sellA.size > 0)
+//     t.assert(book.sellB.size > 0)
+//   } catch (err) {
+//     console.log('### ERROR in base case test', err)
+//   }
+// })
+//
+// test('should submit 10 sells to ledger wile maintaining a sorted set', async (t) => {
+//   const book = new Orderbook()
+//   try {
+//     t.plan(22)
+//     await submitSellAOrders(book)
+//     await submitSellBOrders(book)
+//     t.assert(book.sellA.size === 10)
+//     t.assert(book.sellB.size == 10)
+//     let tempA = 1e16 // arbitrary high number
+//     book.sellA.map(ordr => {
+//       t.assert(ordr.price < tempA)
+//       return tempA = ordr
+//     })
+//     let tempB = 1e16 // arbitrary high number
+//     book.sellB.map(ordr => {
+//       t.assert(ordr.price < tempB)
+//       return tempB = ordr
+//     })
+//   } catch (err) {
+//     console.log('### ERROR in sort test', err)
+//   }
+// })
 
-test('should submit 10 sells to ledger wile maintaining a sorted set', async (t) => {
+test('Should verify hash given a consistent order', async (t) => {
   const book = new Orderbook()
-  try {
-    t.plan(22)
-    await submitSellAOrders(book)
-    await submitSellBOrders(book)
-    t.assert(book.sellA.size === 10)
-    t.assert(book.sellB.size == 10)
-    let tempA = 1e16 // arbitrary high number
-    book.sellA.map(ordr => {
-      t.assert(ordr.price < tempA)
-      return tempA = ordr
-    })
-    let tempB = 1e16 // arbitrary high number
-    book.sellB.map(ordr => {
-      t.assert(ordr.price < tempB)
-      return tempB = ordr
-    })
-  } catch (err) {
-    console.log('### ERROR in sort test', err)
-  }
+  const accounts = await eth.accounts()
+  const order = await randomOrder(accounts[0])
 })
 
 export async function generateSalt() {
@@ -65,13 +71,13 @@ export async function submitSellBOrders(book) {
   if (orderBBatch > 0) await submitSellBOrders(book)
 }
 
-export function randomOrder(from, ) {
-  if (!from) throw new Error('### specify a from public address')
-  return ({
-    from,
-    buy: ,
-    sell: ,
-    price: new BN(Math.floor(Math.random() * 100 + 1)),
-    quantity: new BN(Math.floor(Math.random() * 100 + 1))
-  })
-}
+// export function randomOrder(from) {
+//   if (!from) throw new Error('### specify a from public address')
+//   return ({
+//     from,
+//     price: new BN(Math.floor(Math.random() * 100 + 1)),
+//     quantity: new BN(Math.floor(Math.random() * 100 + 1))
+//     buy ,
+//     sell ,
+//   })
+// }
