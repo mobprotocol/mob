a54f4003b5350eefdcc6675a019aef0836bcdce3
Sean Pollock <seanpollock3344@gmail.com>
2017-10-01 23:37:04 -0700

    adding watcher on state tree

--- state/index.js
+++ state/index.js
@@ -1,16 +1,17 @@
 import level from 'levelup'
 import sublevel from 'level-sublevel'
+const ObservableStore = require('obs-store')
 
 export class State {
   constructor() {
     this.tokenTree = {}
     this.edgeTree  = {}
     this.memory = { tokenTree, edgeTree }
-    this.tokenDisk = sublevel(level('./tokens'))
-    this.edgeDisk = sublevel(level('./edges'))
+    this.store = new ObservableStore(this.memory)
+    this.disk = sublevel(level('./state'))
   }
 
-  async function mutate() {
-
+  watch() {
+    
   }
 }
