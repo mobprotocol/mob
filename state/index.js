51617e55f0405c7e2c2571ce277dd92ddcdd3b86
Sean Pollock <seanpollock3344@gmail.com>
2017-10-02 00:13:27 -0700

    adding testing loop

--- state/index.js
+++ state/index.js
@@ -1,17 +1,43 @@
 import level from 'levelup'
 import sublevel from 'level-sublevel'
-const ObservableStore = require('obs-store')
+import ObservableStore from 'obs-store'
+import { broadcast } from '../server/index'
+import delay from 'async-delay'
 
 export class State {
   constructor() {
     this.tokenTree = {}
     this.edgeTree  = {}
-    this.memory = { tokenTree, edgeTree }
+    this.memory = {
+      tokenTree:this.tokenTree,
+      edgeTree: this.edgeTree
+    }
     this.store = new ObservableStore(this.memory)
     this.disk = sublevel(level('./state'))
+    this.store.subscribe(tree => this.mutate(tree))
   }
 
-  watch() {
-    
+  async mutate(tree) {
+    try {
+      await broadcast(tree)
+    } catch (err) {
+      console.log('### Error in mutation')
+    }
   }
 }
+
+const state = new State()
+
+async function test() {
+  try {
+    state.store.putState({
+      [Date.now()]: Math.random() * 100
+    })
+    await delay(3000)
+    await test()
+  } catch (err) {
+    console.log('### error test loop', err)
+  }
+}
+
+test()
