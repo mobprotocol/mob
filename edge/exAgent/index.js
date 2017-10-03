3282ce2e0f12166d588e96af1da904fb3560b95e
Sean Pollock <seanpollock3344@gmail.com>
2017-10-03 10:03:30 -0700

    adding a inMarket check and loop limiter for testing

--- edge/exAgent/index.js
+++ edge/exAgent/index.js
@@ -2,14 +2,18 @@ import delay from 'await-delay'
 
 export default class ExAgent {
   constructor(params) {
+    console.log('started ex agent')
     this.book = params.orderbook
   }
 
-  async daemon() {
+  async daemon(loops) {
     try {
+      if (loops <= 0) return true
+      console.log('loops', loops)
       await delay(3000)
       await this.match()
-      await this.daemon()
+      loops--
+      await this.daemon(loops)
     } catch (err) {
       console.log('### ERROR in main daemon', err)
     }
@@ -19,8 +23,7 @@ export default class ExAgent {
     try {
       const sides = this.chooseSide()
       const order = this.book[`sell${sides[0]}`].last()
-      console.log('order 1', order)
-      await this.fillOrder(sides, order)
+      this.fillOrder(sides, order)
     } catch (err) {
       console.log('### ERROR in match', err)
     }
@@ -33,19 +36,43 @@ export default class ExAgent {
     else return ['B', 'A']
   }
 
-  fillOrder(sides, order) {
+  async fillOrder(sides, order) {
+    console.log('order', order)
     try {
-      const inverseOrder = this.book[`sell${sides[1]}`].last()  
+      const inverseOrder = this.book[`sell${sides[1]}`].last()
+      if(this.checkInMarket(order.price, inverseOrder.price)) {
+        console.log('### order out of market')
+        return true
+      }
+      // await this.generateSettlementEvent(order, inverseOrder)
     } catch (err) {
       console.log('### ERROR in fillOrder', err)
     }
   }
 
-  async generateSettlementEvent() {
-    try {
+  checkInMarket(price1, price2) {
+    if (price1 > 1/price2) return true
+    else return false
+  }
 
+  // async generateSettlementEvent(order1, order2) {
+  //   try {
+  //     const relativeSize = this.relativeSize(order1, order2)
+  //   } catch (err) {
+  //     console.log('### ERROR in generateSettlementEvent', err)
+  //   }
+  // }
+
+  async relativeSize(order1, order2){
+    try {
+      console.log('order1', order1)
+      console.log('order2', order2)
+      const order1Size = order1.quantity * order1.price
+      const order2Size = order2.quantity * order2.price
+      console.log('order1Sise', order1Size)
+      console.log('order2Size', order2Size)
     } catch (err) {
-      console.log('### ERROR in generateSettlementEvent', err)
+      console.log('### error in releativeSize', err)
     }
   }
 }
