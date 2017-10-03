4b7f0774c49b59604f8cdbe086c2e319eafa90cf
Sean Pollock <seanpollock3344@gmail.com>
2017-10-02 18:24:28 -0700

    getting coinbase via ethjs

--- edge/exAgent/test.js
+++ edge/exAgent/test.js
@@ -1,11 +1,12 @@
 import test from 'tape'
 import ExAgent from './index'
 import Orderbook from '../orderbook/index'
+import HttpProvider from 'ethjs-provider-http'
+import RPC from 'ethjs-rpc'
+const eth = new RPC(new HttpProvider('http://localhost:8545'))
 
 import {
   randomOrder,
-//   submitSellAOrders,
-//   submitSellBOrders
 } from '../orderbook/test'
 
 const book = new Orderbook()
@@ -13,10 +14,22 @@ const agent = new ExAgent({orderbook: book})
 
 test('shold match two orders in the market', async (t) => {
   try {
-    await book.submitSellA(randomOrder())
-    await book.submitSellB(randomOrder())
+    const account = await getCoinbase()
+    console.log('account', account)
+    await book.submitSellA(randomOrder(account))
+    await book.submitSellB(randomOrder(account))
     await agent.daemon()
   } catch (err) {
     console.log('### ERROR in test', err)
   }
 })
+
+async function getCoinbase() {
+  try {
+    const accounts = await eth.sendAsync({ method: 'eth_accounts' })
+    console.log('a')
+    return accounts[0]
+  } catch (err) {
+    console.log('### error in getCoinbase', err)
+  }
+}
